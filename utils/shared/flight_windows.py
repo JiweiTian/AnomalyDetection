b2d6b486003f4a3981bb1f067f59e312d4096baf
yehudapashay <yehudapashay1@gmail.com>
2019-12-31 11:43:31 +0200

    update lstm settings

---
+++ utils/shared/flight_windows.py
@@ -0,0 +1,16 @@
+from utils.shared.lstm_hyper_parameters import LSTM_WINDOW_SIZE
+
+windows = {
+    "flight": [
+        {
+            "lower": 180,
+            "upper": 250,
+        }
+    ],
+    "flight_lstm": [
+        {
+            "lower": 180 - LSTM_WINDOW_SIZE + 1,
+            "upper": 249
+        }
+    ]
+}
